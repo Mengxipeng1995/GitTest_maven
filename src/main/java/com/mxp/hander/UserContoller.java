d0e81322b9d74d8779d9d2a70649bfd4ac2578e5
Mengxipeng1995 <1395124893@qq.com>
2017-06-22 13:26:53 +0800

    test

--- src/main/java/com/mxp/hander/UserContoller.java
+++ src/main/java/com/mxp/hander/UserContoller.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Controller;
  * Created by mxp on 2017/6/22.
  */
 @Controller
-public class UserController {
+public class UserController{
     //12312312343234
     private String index(){
-
+        int s=5;
         return null;
     }
 }
