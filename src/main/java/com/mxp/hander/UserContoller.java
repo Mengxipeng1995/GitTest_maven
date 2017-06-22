d012b70f1b07c4074332bd1ee406d6619af91a59
Mengxipeng1995 <1395124893@qq.com>
2017-06-22 10:55:39 +0800

    添加主要文件

---
+++ src/main/java/com/mxp/hander/UserContoller.java
@@ -0,0 +1,13 @@
+package com.mxp.hander;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by mxp on 2017/6/22.
+ */
+@Controller
+public class UserContoller {
+    private String index(){
+        return null;
+    }
+}
---
+++ src/main/java/com/mxp/services/UserServices.java
@@ -0,0 +1,7 @@
+package com.mxp.services;
+
+/**
+ * Created by mxp on 2017/6/22.
+ */
+public interface UserServices {
+}
